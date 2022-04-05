b292e491054037fa6ec56e49db42ef32a829c739
sallar <sallar.rezaie@crowdbotics.com>
2022-04-05 19:29:23 +0700

    Serialization Update for UserField

--- backend/feedback/serializers.py
+++ backend/feedback/serializers.py
@@ -1,14 +1,16 @@
 from rest_framework import serializers
+
+from home.api.v1.serializers import UserField
 from .models import Feedback
+from users.models import User
 
 
 class FeedbackSerializer(serializers.ModelSerializer):
     """
     A data representation of a User's Feedback to the Admin
     """
+    user = UserField(queryset=User.objects.all(), required=False)
+
     class Meta:
         model = Feedback
         fields = '__all__'
-
-
-#Add User Field
