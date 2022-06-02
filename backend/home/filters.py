e24e8b2d3b79ab2137cf4592629ac14685bd3101
sallar <sallar.rezaie@crowdbotics.com>
2022-06-02 19:43:12 +0700

    Product Min Date Max Date

--- backend/home/filters.py
+++ backend/home/filters.py
@@ -1,11 +1,13 @@
-from django_filters import FilterSet, CharFilter
+from django_filters import FilterSet, CharFilter, DateFilter
 from products.models import Product
 from django.db import models
 
 
 class ProductFilter(FilterSet):
+    min_date = DateFilter(field_name='upload_date', lookup_expr='gte')
+    max_date = DateFilter(field_name='upload_date', lookup_expr='lte')
     styles = CharFilter(lookup_expr='icontains')
 
     class Meta:
          model = Product
-         fields = ['category', 'half_pack_available', 'type', 'brand', 'styles', 'upload_date']
+         fields = ['category', 'half_pack_available', 'type', 'brand', 'styles', 'min_date', 'max_date']
